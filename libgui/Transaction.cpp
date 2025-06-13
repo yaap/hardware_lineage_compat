72a35286a1ce7bdab0f9e614b2402b68b4897a79
unmoved21 <unmoved2121@gmail.com>
2025-06-14 01:34:43 +0200

    libgui: Add SurfaceComposerClient Transaction vtable shim
    Fix Wireless Display (WFD) compilation issues by adding a weak symbol
    implementation for SurfaceComposerClient::Transaction vtable.
    
    This resolves the unresolved symbol: _ZTVN7android21SurfaceComposerClient11TransactionE
    error when building libwfdmmsrc_system.so.
    
    Change-Id: I95469d0237d0017b1be297c9d86d23f157d2db03

---
+++ libgui/Transaction.cpp
@@ -0,0 +1,8 @@
+/*
+ * Copyright (C) 2025 unmoved21
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+extern "C" __attribute__((weak)) void* _ZTVN7android21SurfaceComposerClient11TransactionE[1];
+void* _ZTVN7android21SurfaceComposerClient11TransactionE[1] = { nullptr };
